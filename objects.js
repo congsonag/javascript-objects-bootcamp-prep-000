6f19aa97715ca09f128cb9fae48e5ca814dfe70b
Stephane <son.trinh.dev@gmail.com>
2017-12-29 03:24:12 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -20,3 +20,14 @@ destructivelyUpdateObjectWithKeyAndValue(meals, 'new key', 'new value')
 console.log(meals)
 
 console.log(Object.assign({}, {foo: 'bar' }, {foo: 'new bar'}))
+
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign( {}, object, { [key]: value} )
+}
+
+const recipe = { eggs: 3 }
+
+newObject = updateObjectWithKeyAndValue(recipe, 'tomatoes', 9)
+
+console.log(recipe, newObject)
