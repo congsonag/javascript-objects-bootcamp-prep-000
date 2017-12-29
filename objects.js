2830b174698057649fa709fcb0f0b58b2ddde058
Stephane <son.trinh.dev@gmail.com>
2017-12-29 03:44:52 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = { celinedion: 'my heart will go on', nhwuynh: 'chua co' }
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return Object.assign( {}, playlist, { artist: songTitle })
+  return Object.assign( {}, playlist, { [artist]: songTitle })
 }
