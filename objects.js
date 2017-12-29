2d36f6aa34ac3ad6d7e22043f902a0b27b41f3c6
Stephane <son.trinh.dev@gmail.com>
2017-12-29 03:16:18 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -8,3 +8,13 @@ var anotherMeal = new Object( { breakfast: 'oatmeat', 1: 'is the lonliest number
 
 console.log(anotherMeal[1])
 console.log(anotherMeal.'1')
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  obj[key] = value
+  return object
+}
+
+console.log(meals)
+destructivelyUpdateObjectWithKeyAndValue(meals, 'new key', 'new value')
+
+console.log(meals)
