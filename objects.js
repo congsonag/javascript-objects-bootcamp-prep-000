dfe162fd17374762a3cb9d991188be740f3bb639
Stephane <son.trinh.dev@gmail.com>
2017-12-29 03:48:35 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ var playlist = { celinedion: 'my heart will go on', nhwuynh: 'chua co' }
 function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign(playlist, { [artistName]: songTitle })
 }
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName
+}
