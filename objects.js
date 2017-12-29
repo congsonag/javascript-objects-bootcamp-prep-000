82ad5c2e3ea6a3156af8099d2803dc41fda3619d
Stephane <son.trinh.dev@gmail.com>
2017-12-29 03:05:39 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,9 @@
+var meals = {};
+
+var mealsByConstructor = new Object();
+
+var meals = { breakfast: 'oatmeal'};
+
+var anotherMeal = new Object( { breakfast: 'oatmeat', 1: 'is the lonliest number'})
+
+console.log(anotherMeal[1])
