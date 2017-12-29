233b52512a4a14bfa628b188a9dab6682e581759
Stephane <son.trinh.dev@gmail.com>
2017-12-29 03:08:25 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,4 +7,4 @@ var meals = { breakfast: 'oatmeal'};
 var anotherMeal = new Object( { breakfast: 'oatmeat', 1: 'is the lonliest number'})
 
 console.log(anotherMeal[1])
-console.log(anothermeal.1)
+console.log(anothermeal.breakfast)
